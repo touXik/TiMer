96144ce34008b41b0d6837094257cc6fc661b40d
touXik <toufikmedjber2@gmail.com>
2022-11-26 12:35:19 +0100

    versiond

---
+++ js.js
@@ -0,0 +1,11 @@
+const affich = document.getElementById('counter');
+
+
+
+const updateCounter = async ()=>{ 
+    const data = await fetch('https://api.countapi.xyz/hit/touxik-timer/telecharge');
+    const count = await data.json();
+    affich.innerHTML =count.value+' Télèchargement ';
+}
+
+updateCounter();
